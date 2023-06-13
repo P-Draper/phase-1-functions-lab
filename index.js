825fd65e2d582c71979c7e403a51da1b9aeaa031
P-Draper <draperpa@gmail.com>
2023-06-13 09:24:22 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(someValue) {
+   let hq = 42
+   return Math.abs(hq - someValue)
+  }
+distanceFromHqInBlocks(43) 
+function distanceFromHqInFeet(someValue) {
+   let blocks = distanceFromHqInBlocks(someValue)
+   let feet = 264 * blocks
+   return feet
+}
+distanceFromHqInFeet(43)
+function distanceTravelledInFeet(start, destination) {
+    return Math.abs((start - destination) * 264)
+}
+distanceTravelledInFeet(1, 21)
+function calculatesFarePrice(start, destination) {
+    const distance = distanceTravelledInFeet(start, destination)
+    if (distance <= 400) {
+        return 0
+    } else if (distance <= 2000) {
+        const price = (distance - 400) * 0.02
+        return price
+    } else if (distance <= 2500) {
+        return 25
+    } else {
+        return 'cannot travel that far'
+    }
+}
+calculatesFarePrice()
+
